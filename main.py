75224e9277249dcd50017e4113d476ad03b4c162
Shah <55762593+shahzaibraza37@users.noreply.github.com>
2022-07-19 09:56:23 +0200

    The project has been added to the repository

---
+++ main.py
@@ -0,0 +1,50 @@
+from datetime import datetime
+
+
+date = dict()
+time = dict()
+
+
+def run():
+    try:
+        nOfDataEntry = int(input('How much data do you want to enter?'))
+
+        for dateTimeIdx in range(nOfDataEntry):
+            # input Date
+            inputDate = input('Please enter a date:')
+            try: 
+                datetime.strptime(inputDate, '%d.%m.%Y')
+                date[dateTimeIdx] = inputDate
+            except ValueError as e:
+                print('ERROR' + e)
+                return run()
+            
+            # input Time
+            inputTime = input('Please enter a time:')
+            try: 
+                datetime.strptime(inputTime, '%H:%M')
+                time[dateTimeIdx] = inputTime
+            except ValueError as e:
+                print('ERROR' + e)
+                return run()
+
+
+        checkIdx = 0
+        while 1:
+            dateTimeObj = datetime.now()
+            dateStamp = dateTimeObj.strftime('%d.%m.%Y')
+            timeStamp = dateTimeObj.strftime('%H:%M')
+            idx = 1
+            if dateStamp == date[checkIdx] and timeStamp == time[checkIdx]:
+                print('The time has been reached to perform the task', str(idx), '(' + date[checkIdx] +' - '+ time[checkIdx] +')')
+                idx += 1
+                checkIdx += 1
+            if checkIdx == len(time):
+                result = 'Thank you very much. I will notify them!'
+                print(result)
+                return result
+                # break
+    except:
+        print('Oops! Write an integer here. Try again......!')
+
+run()
---
+++ test_main.py
@@ -0,0 +1,34 @@
+from multiprocessing.sharedctypes import Value
+from unittest import mock
+from unittest import TestCase
+import main
+from datetime import datetime
+
+class Test(TestCase):
+
+    @mock.patch('main.input', create=True)
+    def testPass(self, mocked_input):
+        dateTimeObj = datetime.now()
+        dateStamp = dateTimeObj.strftime('%d.%m.%Y')
+        timeStamp = dateTimeObj.strftime('%H:%M')
+        mocked_input.side_effect = [1, dateStamp, timeStamp]
+        result = main.run()
+        self.assertEqual(result, 'Thank you very much. I will notify them!')
+    
+    @mock.patch('main.input', create=True)
+    def testDate(self, mocked_input):
+        dateTimeObj = datetime.now()
+        dateStamp = dateTimeObj.strftime('%d-%m-%Y')
+        timeStamp = dateTimeObj.strftime('%H:%M')
+        mocked_input.side_effect = [1, dateStamp, timeStamp]
+        result = main.run()
+        self.assertRaises(ValueError)
+
+    @mock.patch('main.input', create=True)
+    def testTime(self, mocked_input):
+        dateTimeObj = datetime.now()
+        dateStamp = dateTimeObj.strftime('%d.%m.%Y')
+        timeStamp = dateTimeObj.strftime('%H-%M')
+        mocked_input.side_effect = [1, dateStamp, timeStamp]
+        result = main.run()
+        self.assertRaises(ValueError)
